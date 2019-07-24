a9c74177635ad457de5aa529baf70cdce77fc917
Shreyas <shre100789@gmail.com>
2019-07-23 22:40:07 -0400

    Added onrowaction on demo component.

--- force-app/main/default/aura/PaginatedDatatableDemo/PaginatedDatatableDemoController.js
+++ force-app/main/default/aura/PaginatedDatatableDemo/PaginatedDatatableDemoController.js
@@ -1,10 +1,28 @@
 ({
     onInit : function(component, event, helper) {
+        var rowActions = [{
+            'label': 'View Details',
+            'iconName': 'utility:zoomin',
+            'name': 'view_details'
+        }];
         component.set("v.accountColumns", [
         {label: 'Id', fieldName: 'Id', type: 'text'},
         {label: 'Account name', fieldName: 'Name', type: 'text'},
-        {label: 'Phone', fieldName: 'Phone', type: 'text'}
+        {label: 'Phone', fieldName: 'Phone', type: 'text'},
+        { type: 'action', typeAttributes: { rowActions: rowActions } }
         ]);
         helper.getAccounts(component, event, helper);
-    }
+    },
+
+    handleRowAction: function (component, event, helper) {
+        var action = event.getParam('action');
+        var row = event.getParam('row');
+        console.log("action: " + action + " row: " + row);
+
+        switch (action.name) {
+            case 'view_details':
+                alert('Showing Details: ' + JSON.stringify(row));
+                break;
+        }
+    },
 })
